d3ebb44d32af034b22b0dcb3bc12bb0c9741fd88
novaskyXP <15570988311@qq.com>
2020-01-09 17:28:28 +0800

    拦截器 restful crud

---
+++ src/main/java/com/atguigu/springbootweb/component/LoginHandlerInterceptor.java
@@ -0,0 +1,37 @@
+package com.atguigu.springbootweb.component;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author xp
+ * @create 2020-01-09 11:37
+ */
+// 拦截器
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object loginUsername = request.getSession().getAttribute("loginUsername");
+        if(loginUsername == null){
+            request.setAttribute("msg","没有权限，请登录");
+            request.getRequestDispatcher("/login.html").forward(request,response);
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
